9d03f677ff8bc7f3c6dbaf81aea8113ff9d7e116
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-09 01:12:42 -0700

    Variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print Numbers
+ *
+ * Description: Prints base 10 single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf(n);
+	}
+	printf("\n");
+	return (0);
+}
