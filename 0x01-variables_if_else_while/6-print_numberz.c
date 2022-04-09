50de016d26fdf430fdd63fffc03fb7312008c13a
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-09 04:53:09 -0700

    Variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers
+ *
+ * Description: Print base 10 single digit numbers using putchar()
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((char)n);
+	}
+	putchar('\n');
+	return (0);
+}
