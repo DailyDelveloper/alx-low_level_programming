d556db44101ad4c8b0dbaffa65ecf86c1745a0d2
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-12 01:06:12 -0700

    Variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations of numbers
+ *
+ * Description: Prints all possible combinations of base numbers
+ * prints "1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + '0');
+		if (a != '9')
+		{
+			putchar(',');
+			puthcar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
