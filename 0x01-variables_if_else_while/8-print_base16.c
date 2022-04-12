468982101c4f07e2686de4a6f835c0113f3086eb
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-11 23:24:23 -0700

    Variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Hexadecimal numbers
+ *
+ * Description: Prints all single digit hexadecimal number using putchar()
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0x1; a < f; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
+}
