868a8889ca8be58a698e0f062bdc4b00a11e6836
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-15 01:53:20 -0700

    Nested Loops

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+
+/**
+ * print_alphabet - Prints alphabet
+ *
+ * Description: Prints alphabet letter in small case
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
