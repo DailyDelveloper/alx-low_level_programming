88696818ee0f310438e838d0b3ef4dda99ca3456
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-19 07:56:36 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+/**
+ * _atoi - converts a string to integer
+ *
+ * Description: Converts a String to an integer
+ * @s: String to be converted to integer
+ * Return: 0 if non found or the integer
+ */
+
+int _atoi(char *s)
+{
+	int num = 0, i;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (!(s[i] > '9' || s[i] < '0'))
+		{
+			num = num * 10 + (s[i] - '0');
+		}
+
+	}
+	return (num);
+
+}
