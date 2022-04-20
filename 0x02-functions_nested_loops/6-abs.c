1707f9075ad924cd5dee827882e50a3f0c319c51
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-20 01:24:48 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+/**
+ * _abs - Compute absolute value of an integer
+ *
+ * Description: Returns absoulte value of a number
+ * @n: The number to calculate absolute value
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
