7553ac4ad890a59d6e6da8828d46506eb946e60f
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-20 08:34:05 -0700

    String Concatination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+/**
+ * _strcat - Concate Strings
+ *
+ * Description: Concate src to dest
+ * @src: first string
+ * @dest: second string
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len1 = 0, len2 = -1;
+
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+
+	do
+	{
+		len2++;
+		dest[len1] = src[len2];
+	} while (src[len2] != '\0');
+
+	return (dest);
+}
