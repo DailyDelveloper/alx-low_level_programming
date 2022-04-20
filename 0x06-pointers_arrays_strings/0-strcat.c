20ecb22103d3dadd16fecf382a91727b3ba438e6
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-20 12:20:41 -0700

    String Concatnation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+
 /**
  * _strcat - Concate Strings
  *
@@ -10,28 +10,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len1 = 0, len2 = -1, len, i;
-	char *res;
+	int len1 = 0, len2 = -1, i;
 
 	while (dest[len1] != '\0')
 	{
 		len1++;
 	}
-	while (src[len2] != '\0')
+	len1--;
+	do
 	{
 		len2++;
-	}
-	len = len1 + len2 + 1;
-	res = (char *) malloc(len);
-
-	for (i = 0; i < len1; i++)
-	{
-		res[i] = dest[i];
-	}
-
-	for (; i <= len; i++)
-	{
-		res[i] = src[i - len2];
-	}
-	return (res);
+		dest[len1] = src[len2];
+		len1++;
+	} while (src[len2] != '\0');
+	return (dest);
 }
