93df67e6407cdcf83ec0f8de4183fbfdf9a4fe8f
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-21 09:06:59 -0700

    String Copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,21 @@
+#include <string.h>
 /**
  * _strncpy - Copy String
  *
  * Description: Copy n number of character from src dest
  * @src: Source String
  * @dest: Destination String
- * Retunr: char
+ * @n: Number of charachters
+ * Return: char
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, len1 = strlen(dest), len2 = strlen(src);
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n && i < len1 && i < len2; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
 	return (dest);
 }
