10d077f687e9b73f344a54832f36d82933c518e3
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-23 11:06:04 +0300

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,15 @@ void times_table(void)
 	{
 		for (b = 0; b <=9; b++)
 		{
-			_putchar((a * b) + '0');
+			if ((a * b) <= 9 )
+			{
+				_putchar((a * b) + '0');
+			}
+			else
+			{
+				_putchar((a * b) / 10, '0');
+				_putchar((a * b) % 10, '0');
+			}
 			if(b != 9)
 			{
 				_putchar(',');
