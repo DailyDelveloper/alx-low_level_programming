c7735a3e5cfd9802dd5f4d5334d00a5584ef5b0c
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-23 10:53:21 +0300

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day
+ *
+ * Description: Prints every minutes of the day
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (!(b > 3 && a == 2))
+			{
+				for (c = 0; c <= 6; c++)
+				{
+					if (c == 6)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar('0');
+						_putchar('\n');
+					}
+					else 
+					{
+						for (d = 0; d <= 9; d++)
+						{
+							_putchar(a + '0');
+							_putchar(b + '0');
+							_putchar(':');
+							_putchar(c + '0');
+							_putchar(d + '0');
+							_putchar('\n');
+						}
+					}
+				}
+			}
+		}
+	}
+}
