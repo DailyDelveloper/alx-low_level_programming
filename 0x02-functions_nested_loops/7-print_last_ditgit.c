3ef021619c31e92716ef0aac1d6c2be82e850ab2
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-23 10:27:47 +0300

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_ditgit.c
@@ -0,0 +1,12 @@
+/**
+ * print_last_digit - Prints last digit of a number
+ *
+ * Description: Prints last digit
+ * @a: The number
+ * Return: int
+ */
+
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
