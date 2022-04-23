8cb8ac577a7d516b64f3e411095fb24077b843b6
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-23 12:41:26 +0300

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,12 +12,16 @@ int main(void)
 	long a = 1, b = 2, i, temp;
 
 	printf("%ld, ", a);
-	for (i = 1; i <= 50; i++)
+	for (i = 1; i < 50; i++)
 	{
-		printf("%ld, ", b);
+		printf("%ld", b);
 		temp = a + b;
 		a = b;
 		b = temp;
+		if(i != 49)
+		{
+			printf(", ");
+		}
 
 	}
 	printf("\n");
