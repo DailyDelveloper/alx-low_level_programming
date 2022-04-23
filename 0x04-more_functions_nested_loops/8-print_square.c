8e64ea83403c2218cd742114620cc86a3985f065
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-23 14:02:54 +0300

    Print Sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Print square
+ *
+ * Description: Print n sized sqaure
+ * @size: Size of the sqaure
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j<= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
