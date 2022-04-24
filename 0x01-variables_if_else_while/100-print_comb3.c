f74f57d9302066b124c77567061db79bb521a317
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-24 17:08:20 +0300

    Print Two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <main.h>
+
+/**
+ * main - Prints combination of two digit
+ *
+ * Description: Prints combination of two digit
+ * Return: int
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			if(i != j)
+			{
+				_putchar(i + '0');
+				_putchar(i + '0');
+
+			}
+			if(i != 8 && j == 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
