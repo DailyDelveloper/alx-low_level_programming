c8654e4b6feba8be4232b084bec73d6f2e56a967
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-25 15:54:28 +0300

    Fill Memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * Description: Fills memory with a given constant byte
+ * @s: the memory holder
+ * @b: the byte 
+ * @n: number of memories to be filled
+ * Retunr: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
