c2182e741e6a53d95744609b0b153985c28954d0
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-26 11:33:19 +0300

    Find Char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+/**
+ * _strchr - Locates character in a string
+ *
+ * Description: Finds and returns pointer of the first 
+ * occuerance of a character
+ * @s: Pointer to a string
+ * @c: The character to be searched
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *res;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			res = s + i;
+			return (res);
+		}
+	}
+
+	return (NULL);
+}
