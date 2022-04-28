b5dd177f11db8167c42b85fa14a5da26acb72baf
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-28 11:16:26 +0300

    Create

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string
+ *
+ * Description: Prints a given string
+ * @s: Pointer to the first char of the  string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s[0]);
+	if (s[1] != '\0')
+	{
+		_puts_recursion(s[1]);
+	}
+}
