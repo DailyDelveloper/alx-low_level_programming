f011fb9f39081a5e0ebf78af8bfe3a34a06f9e07
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-28 13:24:58 +0300

    Create

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+/**
+ * factorial - Return Factorial of a number
+ *
+ * Description: Returns factorial of a number n
+ * @n: Integer 
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
