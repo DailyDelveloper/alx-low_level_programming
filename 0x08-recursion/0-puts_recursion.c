561a04c2dd6c944e781706243328c96260c97dab
DailyDelveloper <mesfineyob51437@gmail.com>
2022-04-28 11:27:02 +0300

    Modified

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,14 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(s[0]);
-	if (s[1] == '\0')
+	
+	if (s[0] != '\0')
 	{
-		_putchar('\n');
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 }
