22b58a8920c8b1ca6c27eefd3b727c5e0f117c1d
DailyDelveloper <mesfineyob51437@gmail.com>
2022-05-03 21:13:59 +0300

    Create

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlin.h>
+/**
+ * _strdup - Duplicate a string
+ *
+ * Description: Duplicates a string
+ * @str: The original string
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	unsigned int size, i;
+       
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = sizeof(str);
+	a = malloc(size);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (size / sizeof(char)); i++)
+	{
+		a[i] = str[i];
+	}
+
+	return (a);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 int _putchar(char);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
