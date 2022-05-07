7b04adaa41899e906a0632d9f725191a8ef811d3
DailyDelveloper <mesfineyob51437@gmail.com>
2022-05-07 21:52:01 +0300

    Create

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ *
+ * Description: Returns a pointer to the allocated memory
+ * @b: memory size
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+
+	str = malloc(b);
+	if( str == NULL)
+	{
+		exit(98);
+	}
+	return (str);
+}
