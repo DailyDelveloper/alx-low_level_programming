acf560e6fb4a4f388b3752c4a543d96d8ba51bb4
DailyDelveloper <mesfineyob51437@gmail.com>
2022-05-16 22:21:18 +0300

    Create

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+/**
+ * _strspn - Gets the length of a prefix substring
+ *
+ * @s: first string
+ * @accept: second string
+ * Return: unsigned int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, count;
+
+	count = 0;
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (a[i] == s[j])
+			{
+				count++; 
+			}
+			else
+			{
+				return (count);
+			}
+		}
+	}
+	return (count);
+}
