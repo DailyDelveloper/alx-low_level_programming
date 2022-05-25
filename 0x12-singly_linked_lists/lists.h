928a5cd1501c3421a600398df2edd2f7ab78995e
Eyob <mesfineyob51437@gmail.com>
2022-05-25 21:35:04 +0300

    Modified

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct list_s{
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length
+ * @next: pointer to the next node
+ * Description: Singly linked list
+ */
+typedef struct list_s
+{
 	char *str;
 	unsigned int len;
 	struct list_s *next;
